49a54fcd902ee55bb5b821f7809c129374c23a6c
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-01-22 20:15:53 -0500

    run test

--- main.py
+++ main.py
@@ -9,22 +9,18 @@ def set_github_action_output(output_name, output_value):
     f.close()    
 
 def main():
-    # using an access token
     token = os.environ["GITHUB_TOKEN"]
-    auth = Auth.Token(token)
+    src = os.environ["INPUT_SOURCE"]
+    dest = os.environ["INPUT_DESTINATION"]
 
-    # Public Web Github
+    auth = Auth.Token(token)
     g = Github(auth=auth)
 
-    # Then play with your Github objects:
-    for repo in g.get_user().get_repos():
-        print(repo.name)
+    repo = g.get_repo("PyGithub/PyGithub")
+    print(repo.name)
 
     # To close connections after use
     g.close()
-
-    src = os.environ["INPUT_SOURCE"]
-    dest = os.environ["INPUT_DESTINATION"]
 
     set_github_action_output('todo', 'Hello world')
     print("TESTING")
