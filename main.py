7663ad8a6eb3325766150b8e718821520e880619
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-01-22 20:45:08 -0500

    run test

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def main():
     auth = Auth.Token(token)
     g = Github(auth=auth)
 
+    print(repository)
     repo = g.get_repo(repository)
     pulls = repo.get_pulls(state='open', sort='created')
     for pr in pulls:
